8b63b79a076561f3c072f2361603673e1af4bc56
oko_jin <dswvgw6412@naver.com>
2025-03-17 15:38:00 +0900

    fix: Promise

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,16 +1,18 @@
-export default function Page({
+export default async function Page({
   params,
   searchParams,
 }: {
-  params: { id: string };
-  searchParams: { id: string; query: string };
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ id: string; query: string }>;
 }) {
+  const pm = await params;
+  const sp = await searchParams;
   return (
     <div>
       <h1>Params</h1>
-      <p>params: {JSON.stringify(params)}</p>
+      <p>params: {JSON.stringify(pm)}</p>
       <h1>Search Params</h1>
-      <p>searchParams: {JSON.stringify(searchParams)}</p>
+      <p>searchParams: {JSON.stringify(sp)}</p>
     </div>
   );
 }
