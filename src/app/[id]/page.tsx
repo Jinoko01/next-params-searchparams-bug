583179f6f4ae42f7246dfd7cf98bc8ced437b414
oko_jin <dswvgw6412@naver.com>
2025-03-18 09:56:56 +0900

    .

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 export interface Props {
   // Route parameter: extracted from /recruitment-boards/list/[id] as 'id'
-  params: { id: string };
+  params: Promise<{ id: string }>;
   // Query parameters: expected to be passed as ?id=12&paramId=8,
   // however, since 'id' is already used in the route, 'id' might not be present in searchParams.
-  searchParams: { id?: string; paramId: string };
+  searchParams: Promise<{ id?: string; paramId: string }>;
 }
 
-export default function ProblemExamplePage({ params, searchParams }: Props) {
+export default async function ProblemExamplePage({
+  params,
+  searchParams,
+}: Props) {
   // Extracting the paramId from the route parameters (e.g., "4")
-  const { id: paramId } = params;
+  const { id: paramId } = await params;
   // The destructuring below attempts to extract 'id' from searchParams, which might be undefined.
-  const { id: searchParamId, paramId: pId } = searchParams;
+  const { id: searchParamId, paramId: pId } = await searchParams;
 
   return (
     <main
